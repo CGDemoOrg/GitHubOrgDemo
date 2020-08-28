9ca0063d9dca3a33efb2fa4646d112b0a2f4b25a
mayurjs26 <43090732+mayurjs26@users.noreply.github.com>
2020-08-28 12:52:53 +0530

    Update MainController.java

--- src/main/java/com/cg/controllers/MainController.java
+++ src/main/java/com/cg/controllers/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 	
-	@GetMapping("/hello")
+	@GetMapping("/hello1")
 	public String helloWorld() {
 		return "Hello World";
 	}
